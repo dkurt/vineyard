fb83e972f8c73ff74ae336d75c1c381d6f4f3b5c
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2022-10-27 20:24:22 +0300

    Use riscv64 cross-compiler from package manager

--- riscv64.toolchain.cmake
+++ riscv64.toolchain.cmake
@@ -1,19 +1,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-if(NOT DEFINED CMAKE_C_COMPILER)
-  find_program(CMAKE_C_COMPILER NAMES riscv64-unknown-linux-gnu-gcc
-                                PATHS /opt/riscv/bin ENV PATH)
-endif()
+set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
 
-if(NOT DEFINED CMAKE_CXX_COMPILER)
-  find_program(CMAKE_CXX_COMPILER NAMES riscv64-unknown-linux-gnu-g++
-                                  PATHS /opt/riscv/bin ENV PATH)
-endif()
-
-set(CMAKE_SYSROOT /opt/riscv/sysroot CACHE PATH "RISC-V sysroot")
-
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
