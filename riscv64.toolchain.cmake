7601ff4e9b052dae5c6d888c903bca2d382809ab
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2022-11-01 14:03:51 +0300

    Use riscv64 cross-compiler from package manager (#16)

--- riscv64.toolchain.cmake
+++ riscv64.toolchain.cmake
@@ -1,20 +1,17 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-if(NOT DEFINED CMAKE_C_COMPILER)
-  find_program(CMAKE_C_COMPILER NAMES riscv64-unknown-linux-gnu-gcc
-                                PATHS /opt/riscv/bin ENV PATH)
-endif()
-
-if(NOT DEFINED CMAKE_CXX_COMPILER)
-  find_program(CMAKE_CXX_COMPILER NAMES riscv64-unknown-linux-gnu-g++
-                                  PATHS /opt/riscv/bin ENV PATH)
-endif()
-
-set(CMAKE_SYSROOT /opt/riscv/sysroot CACHE PATH "RISC-V sysroot")
+set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
 
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Workaround for "undefined reference to `__atomic_compare_exchange_1'": https://stackoverflow.com/a/21144496/6548453
+set(RISCV_LINKER_FLAGS "-Wl,--no-as-needed -latomic")
+set(CMAKE_SHARED_LINKER_FLAGS "${RISCV_LINKER_FLAGS} ${CMAKE_SHARED_LINKER_FLAGS}")
+set(CMAKE_MODULE_LINKER_FLAGS "${RISCV_LINKER_FLAGS} ${CMAKE_MODULE_LINKER_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS    "${RISCV_LINKER_FLAGS} ${CMAKE_EXE_LINKER_FLAGS}")
